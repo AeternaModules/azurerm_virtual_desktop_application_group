de526d91561dfb8555a70ee61c237b52e2abfe41
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:16:39 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_desktop_application_groups" {
+  description = "All virtual_desktop_application_group resources"
+  value       = azurerm_virtual_desktop_application_group.virtual_desktop_application_groups
+}
+
