8bc1ee5fdc6791be60f06e936676f35e6cad23eb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "virtual_desktop_application_groups" {
-  description = "All virtual_desktop_application_group resources"
-  value       = azurerm_virtual_desktop_application_group.virtual_desktop_application_groups
-}
 output "virtual_desktop_application_groups_default_desktop_display_name" {
-  description = "List of default_desktop_display_name values across all virtual_desktop_application_groups"
-  value       = [for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : v.default_desktop_display_name]
+  description = "Map of default_desktop_display_name values across all virtual_desktop_application_groups, keyed the same as var.virtual_desktop_application_groups"
+  value       = { for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : k => v.default_desktop_display_name }
 }
 output "virtual_desktop_application_groups_description" {
-  description = "List of description values across all virtual_desktop_application_groups"
-  value       = [for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : v.description]
+  description = "Map of description values across all virtual_desktop_application_groups, keyed the same as var.virtual_desktop_application_groups"
+  value       = { for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : k => v.description }
 }
 output "virtual_desktop_application_groups_friendly_name" {
-  description = "List of friendly_name values across all virtual_desktop_application_groups"
-  value       = [for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : v.friendly_name]
+  description = "Map of friendly_name values across all virtual_desktop_application_groups, keyed the same as var.virtual_desktop_application_groups"
+  value       = { for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : k => v.friendly_name }
 }
 output "virtual_desktop_application_groups_host_pool_id" {
-  description = "List of host_pool_id values across all virtual_desktop_application_groups"
-  value       = [for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : v.host_pool_id]
+  description = "Map of host_pool_id values across all virtual_desktop_application_groups, keyed the same as var.virtual_desktop_application_groups"
+  value       = { for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : k => v.host_pool_id }
 }
 output "virtual_desktop_application_groups_location" {
-  description = "List of location values across all virtual_desktop_application_groups"
-  value       = [for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : v.location]
+  description = "Map of location values across all virtual_desktop_application_groups, keyed the same as var.virtual_desktop_application_groups"
+  value       = { for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : k => v.location }
 }
 output "virtual_desktop_application_groups_name" {
-  description = "List of name values across all virtual_desktop_application_groups"
-  value       = [for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : v.name]
+  description = "Map of name values across all virtual_desktop_application_groups, keyed the same as var.virtual_desktop_application_groups"
+  value       = { for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : k => v.name }
 }
 output "virtual_desktop_application_groups_resource_group_name" {
-  description = "List of resource_group_name values across all virtual_desktop_application_groups"
-  value       = [for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all virtual_desktop_application_groups, keyed the same as var.virtual_desktop_application_groups"
+  value       = { for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : k => v.resource_group_name }
 }
 output "virtual_desktop_application_groups_tags" {
-  description = "List of tags values across all virtual_desktop_application_groups"
-  value       = [for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : v.tags]
+  description = "Map of tags values across all virtual_desktop_application_groups, keyed the same as var.virtual_desktop_application_groups"
+  value       = { for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : k => v.tags }
 }
 output "virtual_desktop_application_groups_type" {
-  description = "List of type values across all virtual_desktop_application_groups"
-  value       = [for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : v.type]
+  description = "Map of type values across all virtual_desktop_application_groups, keyed the same as var.virtual_desktop_application_groups"
+  value       = { for k, v in azurerm_virtual_desktop_application_group.virtual_desktop_application_groups : k => v.type }
 }
 
